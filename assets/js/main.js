a1ceb06f5aee90da1eaa4eb347dcdec565badcea
Jhonattan Sueoka <jhonattansueoka@gmail.com>
2022-08-26 14:49:54 -0300

    finalizando consulta de api

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,42 @@
-document.addEventListener("DOMContentLoaded", function(e){
+// document.addEventListener("DOMContentLoaded", function(e){
 
-    // function setCepMask() {
-    //     const cepInput = [
-    //         document.querySelector('input[name="cep"]')
-    //     ];
-    //     for(input of cepInput)
-    //     if (input.length > 0) input.mask("00000-000");
-    // }
+//     // setCepMask();
 
-    // setCepMask();
-
-    function buscarEndereco(){
-
-        let cep = document.querySelector('#cep').value;
     
-        if(cep.length != 8){
-            console.log("Digite um CEP válido.");
-        }
-    
-        let url = "viacep.com.br/ws/" + cep + "/json/";
+// })
 
-        console.log(cep);
-    
+// // function setCepMask() {
+//     //     const cepInput = [
+//     //         document.querySelector('input[name="cep"]')
+//     //     ];
+//     //     for(input of cepInput)
+//     //     if (input.length > 0) input.mask("00000-000");
+//     // }
+
+function buscarEndereco(){
+
+    let cep = document.querySelector('#cep').value;
+
+    if(cep.length != 8){
+       document.querySelector('.invalid').classList.add('show');
     }
-})
 
+    let url = "https://viacep.com.br/ws/" + cep + "/json/";
+    
+    fetch(url).then(function(response){
+        response.json().then(apresentaEndereco);
+    })
+}
+function apresentaEndereco(dados){
+    let resposta = document.querySelector('.resposta');
 
-
+    if(dados.erro){
+        resposta.innerHTML = "<p>Endereço não localizado!</p>";
+    }
+    else{
+        resposta.innerHTML =`<p>Endereço: ${dados.logradouro}</p>
+                            <p>Complemento: ${dados.complemento}</p>
+                            <p>Bairro:${dados.bairro}</p>
+                            <p>UF:${dados.uf}</p>`
+    }
+}
